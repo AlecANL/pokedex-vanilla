e8c2d6eba10f8b6288878934f8c951ae71c4e135
alec <alexeialvarez4@gmail.com>
2021-06-13 14:08:57 -0600

    :hammer: [UPDATE] try to fix problems with pwa in production

--- src/js/app.js
+++ src/js/app.js
@@ -37,7 +37,7 @@ $prevPokemon.addEventListener('click', getPreviusPokemon);
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/sw.js')
+    .register('sw.js', { scope: 'https://alecanl.github.io/pokedex-vanilla/' })
     .then(register => console.log(`installed succesfully: ${register}`))
     .catch(error => console.warn(`whoops: ${error}`));
 } else {
